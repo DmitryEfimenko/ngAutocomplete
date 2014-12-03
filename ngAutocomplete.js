c145130f6754b8648de8e5d5823199a81efd1e3b
Dmitry A. Efimenko <dmitrief@gmail.com>
2014-12-03 01:52:52 -0700

    removed isolate scope

--- ngAutocomplete.js
+++ ngAutocomplete.js
@@ -12,7 +12,7 @@
  * 
  *   Required attributes
  *   [ng-autocomplete]: Specifies the directive
- *   [ng-model]: Set it to [details].formattedAddress so that displayed address is updated based on user's selection
+ *   [ng-model]: Set initial value for the textbox
  *   [details]: Specifies result object which is a bit flattened "google place" object, where properties will be set to the address types.
  *              For more info about google types, see: https://developers.google.com/maps/documentation/geocoding/#Types
  *   
@@ -32,8 +32,8 @@
  */
 
 angular.module("ngAutocomplete", [])
-    .directive('ngAutocomplete', [
-        function () {
+    .directive('ngAutocomplete', ['$parse',
+        function ($parse) {
             function convertPlaceToFriendlyObject(place) {
                 var result = undefined;
                 if (place) {
@@ -54,29 +54,33 @@ angular.module("ngAutocomplete", [])
             return {
                 restrict: 'A',
                 require: 'ngModel',
-                scope: {
-                    details: '=',
-                    options: '=',
-                    validateFn: '&'
-                },
                 link: function ($scope, $element, $attrs, $ctrl) {
+                    if (!angular.isDefined($attrs.details)) {
+                        throw '<ng-autocomplete> must have attribute [details] assigned to store full address object';
+                    }
+
+                    var getDetails = $parse($attrs.details);
+                    var setDetails = getDetails.assign;
+                    var getOptions = $parse($attrs.options);
+
                     //options for autocomplete
                     var opts;
 
                     //convert options provided to opts
                     var initOpts = function () {
                         opts = {};
-                        if ($scope.options) {
-                            if ($scope.options.types) {
+                        if (angular.isDefined($attrs.options)) {
+                            var options = getOptions($scope);
+                            if (options.types) {
                                 opts.types = [];
-                                opts.types.push($scope.options.types);
+                                opts.types.push(options.types);
                             }
-                            if ($scope.options.bounds) {
-                                opts.bounds = $scope.options.bounds;
+                            if (options.bounds) {
+                                opts.bounds = options.bounds;
                             }
-                            if ($scope.options.country) {
+                            if (options.country) {
                                 opts.componentRestrictions = {
-                                    country: $scope.options.country
+                                    country: options.country
                                 };
                             }
                         }
@@ -85,24 +89,28 @@ angular.module("ngAutocomplete", [])
                     //create new autocomplete
                     //reinitializes on every change of the options provided
                     var newAutocomplete = function () {
-                        $scope.gPlace = new google.maps.places.Autocomplete($element[0], opts);
-                        google.maps.event.addListener($scope.gPlace, 'place_changed', function () {
+                        var gPlace = new google.maps.places.Autocomplete($element[0], opts);
+                        google.maps.event.addListener(gPlace, 'place_changed', function () {
                             $scope.$apply(function () {
-                                var place = $scope.gPlace.getPlace();
-                                $scope.details = convertPlaceToFriendlyObject(place);
+                                var place = gPlace.getPlace();
+                                var details = convertPlaceToFriendlyObject(place);
+                                setDetails($scope, details);
+                                $ctrl.$setViewValue(details.formattedAddress);
                                 $ctrl.$validate();
                             });
-                            if ($ctrl.$valid && $scope.validateFn) {
+                            if ($ctrl.$valid && angular.isDefined($attrs.validateFn)) {
                                 $scope.$apply(function () {
-                                    $scope.validateFn();
+                                    $scope.$eval($attrs.validateFn);
                                 });
                             }
                         });
                     };
+                    newAutocomplete();
 
                     $ctrl.$validators.parse = function (value) {
-                        var valid = ($attrs.required == true && $scope.details != undefined && $scope.details.lat != undefined) ||
-                            (!$attrs.required && ($scope.details == undefined || $scope.details.lat == undefined) && $element.val() != '');
+                        var details = getDetails($scope);
+                        var valid = ($attrs.required == true && details != undefined && details.lat != undefined) ||
+                            (!$attrs.required && (details == undefined || details.lat == undefined) && $element.val() != '');
                         return valid;
                     };
 
@@ -114,29 +122,29 @@ angular.module("ngAutocomplete", [])
                     });
 
                     //watch options provided to directive
-                    $scope.watchOptions = function () {
-                        return $scope.options;
-                    };
-                    $scope.$watch($scope.watchOptions, function () {
-                        initOpts();
-                        newAutocomplete();
-                        $element[0].value = '';
-                    }, true);
+                    if (angular.isDefined($attrs.options)) {
+                        $scope.$watch($attrs.options, function() {
+                            initOpts();
+                            newAutocomplete();
+                        });
+                    }
 
                     // user typed something in the input - means an intention to change address, which is why
                     // we need to null out all fields for fresh validation
                     $element.on('keyup', function (e) {
                         //          chars 0-9, a-z                        numpad 0-9                   backspace         delete           space
                         if ((e.which >= 48 && e.which <= 90) || (e.which >= 96 && e.which <= 105) || e.which == 8 || e.which == 46 || e.which == 32) {
-                            if ($scope.details) {
-                                for (var property in $scope.details) {
-                                    if ($scope.details.hasOwnProperty(property) && property != 'formattedAddress') {
-                                        delete $scope.details[property];
+                            var details = getDetails($scope);
+                            if (details != undefined) {
+                                for (var property in details) {
+                                    if (details.hasOwnProperty(property) && property != 'formattedAddress') {
+                                        delete details[property];
                                     }
                                 }
+                                setDetails($scope, details);
                             }
                             if ($ctrl.$valid) {
-                                $scope.$apply(function() {
+                                $scope.$apply(function () {
                                     $ctrl.$setValidity('parse', false);
                                 });
                             }
